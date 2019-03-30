0a17cb5e98b08fd24f6993366b8de01a51c720b7
David Redondo <rdnd201@gmail.com>
2019-03-30 01:51:46 +0100

    how it works section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import AnonRoute from "./components/AnonRoute";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HowItWorks from "./components/HowItWorks";
 import Private from "./pages/Private";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
@@ -17,6 +18,7 @@ class App extends Component {
         <div>
           <Navbar />
           <Hero />
+          <HowItWorks />
           <Switch>
             <AnonRoute path="/signup" component={Signup} />
             <AnonRoute path="/login" component={Login} />
